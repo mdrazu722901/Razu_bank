936c7cace073600a99fd777002d79a009d4b1c1f
mdraju <md raju>
2021-01-28 21:44:20 +0600

    added bank

---
+++ script/script.js
@@ -0,0 +1,43 @@
+// log in button ========================================================
+const loginbtn = document.getElementById('login')
+loginbtn.addEventListener('click', function() {
+    const login_area = document.getElementById('login-area');
+    login_area.style.display = 'none';
+    const transaction_area = document.getElementById('transaction_area');
+    transaction_area.style.display = "block";
+})
+
+// deposit button =========================================================
+const deposit = document.getElementById('Add-deposit');
+deposit.addEventListener('click', function() {
+    const depositNumber = getInputNumber("deposit_Amount")
+
+    updateTOSpanText("current_Deposit", depositNumber);
+    updateTOSpanText("current_balance", depositNumber);
+
+
+    document.getElementById("deposit_Amount").value = "";
+
+})
+
+//withdraw handler event =======================================
+
+const withdrawbtn = document.getElementById("Add-withdraw");
+withdrawbtn.addEventListener("click", function() {
+    const withdrawAmount = getInputNumber("withdraw_Amount");
+    console.log(withdrawAmount);
+
+})
+
+function getInputNumber(id) {
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
+}
+
+function updateTOSpanText(id, n) {
+    const currentBalance = document.getElementById(id).innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = n + currentBalanceNumber;
+    document.getElementById(id).innerText = totalBalance;
+}
